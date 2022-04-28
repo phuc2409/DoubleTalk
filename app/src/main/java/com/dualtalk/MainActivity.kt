b956458a6143e9286ecd276dbdb5f8033f0fa0d5
Phúc <quangphuc2409@gmail.com>
2022-04-28 11:05:01 +0700

    1 chút data binding

--- app/src/main/java/com/dualtalk/MainActivity.kt
+++ app/src/main/java/com/dualtalk/MainActivity.kt
@@ -2,10 +2,16 @@ package com.dualtalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dualtalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        dataBinding.textViewHello.text = "Xin chào"
     }
 }
