c5d408a8cec9c02e30b820eebdefee836b2142de
ducli08 <87600581+ducli08@users.noreply.github.com>
2022-04-29 14:37:59 +0700

    Login,SignUp(xml)

---
+++ app/src/main/java/com/dualtalk/View/SiugnUp.kt
@@ -0,0 +1,12 @@
+package com.dualtalk.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dualtalk.R
+
+class SiugnUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_siugn_up)
+    }
+}
