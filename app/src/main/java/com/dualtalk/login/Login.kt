a802afa4cce26986c4e959497fb93400e0114d6f
ducli08 <87600581+ducli08@users.noreply.github.com>
2022-04-30 16:21:10 +0700

    loginnew

--- app/src/main/java/com/dualtalk/login/Login.kt
+++ app/src/main/java/com/dualtalk/login/Login.kt
@@ -3,6 +3,7 @@ package com.dualtalk.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -13,35 +14,45 @@ import com.dualtalk.signup.SignupActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
-    private lateinit var databiding : ActivityLoginBinding
-    private lateinit var viewModel: Login_ViewModel
+//    private lateinit var databiding : ActivityLoginBinding
+//    private lateinit var viewModel: Login_ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        databiding = DataBindingUtil.setContentView(this ,R.layout.activity_login)
-        viewModel = ViewModelProvider(this).get(Login_ViewModel::class.java)
+        setContentView(R.layout.activity_login)
+        signUp.setOnClickListener {
+            signUp.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            signIn.background = null
+            signuplayout.visibility = View.VISIBLE
+            loginlayout.visibility = View.GONE
 
-
-        databiding.btndangnhap.setOnClickListener{
-            viewModel.login(databiding.editTextEmail.text.toString() , databiding.editTextPassword.text.toString())
         }
+        signIn.setOnClickListener {
+            signIn.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            signUp.background = null
+            loginlayout.visibility = View.VISIBLE
+            signuplayout.visibility = View.GONE
 
-        databiding.SignUp.setOnClickListener{
-            val intent = Intent(this@Login , SignupActivity::class.java)
-            startActivity(intent)
         }
-
-        viewModel.UiLoginState.observe(this){
-            if(it == Login_ViewModel.LoginState.Success){
-                Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
-                var intent : Intent = Intent(this@Login, MainActivity::class.java)
-                startActivity(intent)
-            }
-
-            if(it == Login_ViewModel.LoginState.Fail){
-                Toast.makeText(this, "Danh nhap that bai", Toast.LENGTH_SHORT).show()
-            }
-        }
+//        databiding = DataBindingUtil.setContentView(this ,R.layout.activity_login)
+//        viewModel = ViewModelProvider(this).get(Login_ViewModel::class.java)
+//
+//
+//        databiding.signin.setOnClickListener{
+//            viewModel.login(databiding.email.text.toString() , databiding.pasword.text.toString())
+//        }
+//
+//        viewModel.UiLoginState.observe(this){
+//            if(it == Login_ViewModel.LoginState.Success){
+//                Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+//                var intent : Intent = Intent(this@Login, MainActivity::class.java)
+//                startActivity(intent)
+//            }
+//
+//            if(it == Login_ViewModel.LoginState.Fail){
+//                Toast.makeText(this, "Danh nhap that bai", Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
 
 
--- app/src/main/java/com/dualtalk/login/Login_ViewModel.kt
+++ app/src/main/java/com/dualtalk/login/Login_ViewModel.kt
@@ -1,41 +1,41 @@
-package com.dualtalk.login
-
-import androidx.databinding.Observable
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class Login_ViewModel : ViewModel() , Observable{
-
-    sealed class LoginState{
-        object Loading : LoginState()
-        object Success : LoginState()
-        object Fail : LoginState()
-    }
-
-    val UiLoginState = MutableLiveData<LoginState>()
-
-    fun login(email:String , password:String){
-        val user = User(email , password)
-        UiLoginState.postValue(LoginState.Loading)
-
-        if(user.email.equals("dat@gmail.com") && user.password.equals("123456")){
-            UiLoginState.postValue(LoginState.Success)
-        }
-
-        else {
-            UiLoginState.postValue(LoginState.Fail)
-        }
-    }
-
-
-
-
-    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
-        TODO("Not yet implemented")
-    }
-
-}
+//package com.dualtalk.login
+//
+//import androidx.databinding.Observable
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//
+//class Login_ViewModel : ViewModel() , Observable{
+//
+//    sealed class LoginState{
+//        object Loading : LoginState()
+//        object Success : LoginState()
+//        object Fail : LoginState()
+//    }
+//
+//    val UiLoginState = MutableLiveData<LoginState>()
+//
+//    fun login(email:String , password:String){
+//        val user = User(email , password)
+//        UiLoginState.postValue(LoginState.Loading)
+//
+//        if(user.email.equals("dat@gmail.com") && user.password.equals("123456")){
+//            UiLoginState.postValue(LoginState.Success)
+//        }
+//
+//        else {
+//            UiLoginState.postValue(LoginState.Fail)
+//        }
+//    }
+//
+//
+//
+//
+//    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
