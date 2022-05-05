a2ce0eed906fb43488a0cb3bd83cf835c6436e3f
Daiappweb <92419330+Daiappweb@users.noreply.github.com>
2022-05-05 16:58:17 +0700

    Update UserModel.kt

--- app/src/main/java/com/dualtalk/activity/login/UserModel.kt
+++ app/src/main/java/com/dualtalk/activity/login/UserModel.kt
@@ -1,9 +1,15 @@
 package com.dualtalk.activity.login
 
 class UserModel(email: String, password: String, name: String) {
-    var email: String
+   var email: String
+    get() = this.email
+        set(value) {email = value}
     var password: String
+        get() =this.password
+        set(value) {password= value}
     var name: String
+        get() =this.name
+        set(value) {name = value}
 
     constructor(email: String, password: String) : this(email, password, "Unknown") {
         this.email = email
