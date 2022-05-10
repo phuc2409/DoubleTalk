edc1c2dbbcfff1419fa25331ff7505d4c63ac95a
Nguyễn Tiến Đạt <tiendat08092001@gmail.com>
2022-05-10 22:30:25 +0700

    fix ko hiển thị avartar

--- app/src/main/java/com/dualtalk/activity/searchuser/MUser.kt
+++ app/src/main/java/com/dualtalk/activity/searchuser/MUser.kt
@@ -1,13 +1,14 @@
 package com.dualtalk.activity.searchuser
 
-class MUser (email:String, username : String, url : String ) {
+class MUser (id : String , email:String, username : String, url : String ) {
      var mEmail : String
      var mUsername : String
      var UrlImageUser: String
-
+     var id : String
     init {
         this.mEmail = email
         this.mUsername = username
         this.UrlImageUser = url
+        this.id = id
     }
 }
--- app/src/main/java/com/dualtalk/activity/searchuser/SearchUserViewModel.kt
+++ app/src/main/java/com/dualtalk/activity/searchuser/SearchUserViewModel.kt
@@ -23,11 +23,13 @@ class SearchUserViewModel : ViewModel(), Observable {
     fun getListUser() {
         db.collection("users").get().addOnSuccessListener { result ->
             for (user in result) {
-                val email = user.getString("email").toString()
-                val name = user.getString("fullName").toString()
-                val url = user.getString("url").toString()
-                mlist.add(MUser(email, name, url))
+                val email = user.getString("email").toString().trim()
+                val name = user.getString("fullName").toString().trim()
+                val url = user.getString("imgUrl").toString().trim()
+                val id = user.getString("id").toString().trim()
+                mlist.add(MUser(id ,email, name, url))
             }
+            mlist.add(MUser("1" , "dat@gmail.com" , "Đạt Nguyễn" , "https://firebasestorage.googleapis.com/v0/b/demo2021-ae092.appspot.com/o/Picture%2Fcontent%3A%2Fcom.android.providers.media.documents%2Fdocument%2Fimage%253A24?alt=media&token=3a149aa5-c97e-4858-a295-a5a9e59b3399"))
             uiState.postValue(SearchUserState.Success)
             Log.d("mlist", "Đã có size = ${mlist.size}")
         }
