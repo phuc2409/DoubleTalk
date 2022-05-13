dceeb2cf1c9714190b7b8c6c90c00a8d36c9c4fe
Phúc <quangphuc2409@gmail.com>
2022-05-13 16:37:58 +0700

    Nâng cấp giao diện chat

--- app/src/main/java/com/dualtalk/activity/chat/MessageAdapter.kt
+++ app/src/main/java/com/dualtalk/activity/chat/MessageAdapter.kt
@@ -1,6 +1,5 @@
 package com.dualtalk.activity.chat
 
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,15 +7,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dualtalk.R
 import com.dualtalk.common.CurrentUser
 import com.dualtalk.helper.DateTimeHelper
-import kotlinx.android.synthetic.main.item_message.view.*
+import kotlinx.android.synthetic.main.item_message_send.view.*
 import kotlin.collections.ArrayList
 
 class MessageAdapter(private val list: ArrayList<MessageModel>) :
     RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
-        return ViewHolder(view)
+        return if (viewType == 0) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_message_send, parent, false)
+            ViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_message_receive, parent, false)
+            ViewHolder(view)
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -28,13 +34,16 @@ class MessageAdapter(private val list: ArrayList<MessageModel>) :
         return list.size
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if (list[position].sendId == CurrentUser.id) {
+            0
+        } else {
+            1
+        }
+    }
+
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         fun bindData(item: MessageModel) {
-            if (item.sendId == CurrentUser.id) {
-                itemView.tvMessage.gravity = Gravity.END
-                itemView.tvCreatedAt.gravity = Gravity.END
-            }
-
             itemView.tvMessage.text = item.message
             if (item.createdAt != null) {
                 itemView.tvCreatedAt.text = DateTimeHelper.timestampToDateTimeString(item.createdAt)
