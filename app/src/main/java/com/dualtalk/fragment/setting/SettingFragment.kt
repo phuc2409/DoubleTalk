5a7d21209e8cb06342f3126da4f985db854577f1
ducli08 <87600581+ducli08@users.noreply.github.com>
2022-05-13 11:12:34 +0700

    commit

--- app/src/main/java/com/dualtalk/fragment/setting/SettingFragment.kt
+++ app/src/main/java/com/dualtalk/fragment/setting/SettingFragment.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +18,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -32,6 +36,7 @@ import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.fragment_setting.*
+import java.security.Permission
 import java.util.prefs.Preferences
 
 // TODO: Rename parameter arguments, choose names that match
@@ -93,14 +98,6 @@ class SettingFragment : Fragment() {
             startActivity(intent)
         }
 
-
-
-
-
-
-
-
-
         viewModel.uiState.observe(viewLifecycleOwner){
             if(it == SettingFragmentViewModel.ChooseImageAvatar.Success){
                 Toast.makeText(mview.context , "Up anh thanh cong va link anh la ${viewModel.urlUserAvartar} " , Toast.LENGTH_SHORT).show()
@@ -111,6 +108,10 @@ class SettingFragment : Fragment() {
             else{
                 Toast.makeText(mview.context , "Up anh ko thanh cong" , Toast.LENGTH_SHORT).show()
             }
+        }
+        UserAvartar.setOnClickListener {
+            Toast.makeText(requireContext(),"click",Toast.LENGTH_SHORT).show()
+
         }
 
 
