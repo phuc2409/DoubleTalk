d26427e011e65b3f9699d9cb272a3ff43f5b88f9
Phúc <quangphuc2409@gmail.com>
2022-05-14 13:20:55 +0700

    Xoá file không dùng

--- app/src/main/java/com/dualtalk/activity/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/dualtalk/activity/forgotpassword/ForgotPasswordActivity.kt
@@ -2,44 +2,39 @@ package com.dualtalk.activity.forgotpassword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.dualtalk.R
-import com.dualtalk.databinding.ActivityForgotPassword2Binding
-import com.google.android.material.textfield.TextInputEditText
+import com.dualtalk.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
-    private lateinit var databiding : ActivityForgotPassword2Binding
-    lateinit var viewmodel : ForgotPasswordViewModel
-
+    private lateinit var dataBinding: ActivityForgotPasswordBinding
+    lateinit var viewModel: ForgotPasswordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        databiding = DataBindingUtil.setContentView(this ,R.layout.activity_forgot_password2)
-        databiding.lifecycleOwner = this
-        viewmodel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_forgot_password)
+        dataBinding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
 
-        val btnSend = findViewById<Button>(R.id.btnSendLinkToResetPassword)
-        val edtEmail = findViewById<TextInputEditText>(R.id.ForgotPassword_email)
-        btnSend.setOnClickListener{
-            var emailadress = edtEmail.text.toString().trim()
-            viewmodel.SendLinkToResetPassword(emailadress)
+        dataBinding.btnSend.setOnClickListener {
+            val email = dataBinding.edtEmail.text.toString().trim()
+            viewModel.SendLinkToResetPassword(email)
         }
 
-        viewmodel.forgotpassState.observe(this){
-            if(it == ForgotPasswordViewModel.forgotPassState.Success){
-                Toast.makeText(this, "Please check your email to reset password!", Toast.LENGTH_SHORT).show()
+        viewModel.forgotpassState.observe(this) {
+            if (it == ForgotPasswordViewModel.forgotPassState.Success) {
+                Toast.makeText(
+                    this,
+                    "Please check your email to reset password!",
+                    Toast.LENGTH_SHORT
+                ).show()
                 finish()
             }
-            if(it == ForgotPasswordViewModel.forgotPassState.Fail){
+            if (it == ForgotPasswordViewModel.forgotPassState.Fail) {
                 Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show()
             }
         }
-
-
-
-
     }
 }
